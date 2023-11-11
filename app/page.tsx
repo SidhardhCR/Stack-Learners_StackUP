cfc0ba8282d3afce18f621035113c4aeac49c3e6
SidhardhCR <sidhardhcr007@gmail.com>
2023-11-11 21:37:25 +0530

    added filter method

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 import { db } from "./firebase";
 import { collection, getDocs } from "firebase/firestore";
 import React, { useState, useEffect } from "react";
+import { Form } from "react-bootstrap";
+import InputGroupText from "react-bootstrap/esm/InputGroupText";
 
 async function FetchDataFromFireStore() {
   const querySnapshot = await getDocs(collection(db, "messages"));
@@ -19,6 +21,7 @@ async function FetchDataFromFireStore() {
 }
 
 export default function Home() {
+  const [search , setSearch] = useState('');
   const [eventData, setEventData] = useState<Array<any> | null>(null);
 
   useEffect(() => {
@@ -31,14 +34,23 @@ export default function Home() {
     })();
   }, []);
   return (
-    <main className=" bg-white">
-      <div className="min-h-screen min-w-full  bg-neutral-100 container py-20 px-8">
+    <main className=" bg-white ">
+    
+      <div className="min-h-screen min-w-full  bg-neutral-100 container py-20 px-8 ">
+     
         <h1 className="text-5xl font-bold text-center text-black pb-10">
           Events
         </h1>
+        <div className="pb-7 flex justify-center w-full">
+        <Form >
+          <InputGroupText className="my-3">
+             <Form.Control onChange={(e)=>setSearch(e.target.value)} placeholder="Search Events" className="bg-white text-black border border-info rounded-circle  " size="lg" />
+          </InputGroupText>
+        </Form>
+        </div>
         <div className="grid lg:grid-cols-4 text-black gap-16">
           {eventData &&
-            eventData.map((data) => {
+            eventData.filter((data)=>{return search.toLowerCase() === '' ? data : data.eventname.toLowerCase().includes(search.toLowerCase())}).map((data) => {
               return <div key={data.id}><div className="card card-compact lg:w-80 h-80  bg-white shadow-md rounded-lg' shadow-xl">
               <figure><img src="/card.jpg" alt="Shoes" /></figure>
               <div className="card-body text-black">
